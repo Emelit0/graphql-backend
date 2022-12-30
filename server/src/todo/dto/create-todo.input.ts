f877acd9771b8af26dff3a76ddf28abecc2d74ef
Emelit0 <stollk811@gmail.com>
2022-12-30 20:36:04 +0100

    setup crud and db migration

--- server/src/todo/dto/create-todo.input.ts
+++ server/src/todo/dto/create-todo.input.ts
@@ -2,6 +2,6 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateTodoInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  title: string;
 }
--- server/src/todo/dto/update-todo.input.ts
+++ server/src/todo/dto/update-todo.input.ts
@@ -5,4 +5,7 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 export class UpdateTodoInput extends PartialType(CreateTodoInput) {
   @Field(() => Int)
   id: number;
+
+  @Field()
+  title: string;
 }
